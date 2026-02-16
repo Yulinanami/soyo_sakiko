2eb8e28efe9ff24ed9fa097e9b4450a6ce3d51a6
Yulinanami <yiy27532@gmail.com>
2026-02-16 16:00:01 +0800

    修复开启代理软件TUN模式导致的请求被转发到代理的问题

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'node:path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@composables': path.resolve(__dirname, 'src/composables'),
-      '@services': path.resolve(__dirname, 'src/services'),
-      '@stores': path.resolve(__dirname, 'src/stores'),
-      '@app-types': path.resolve(__dirname, 'src/types'),
-      '@views': path.resolve(__dirname, 'src/views'),
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@composables": path.resolve(__dirname, "src/composables"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@stores": path.resolve(__dirname, "src/stores"),
+      "@app-types": path.resolve(__dirname, "src/types"),
+      "@views": path.resolve(__dirname, "src/views"),
     },
+  },
+  server: {
+    host: "127.0.0.1",
   },
 });
